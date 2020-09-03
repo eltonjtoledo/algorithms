905fa095aa6407095939616705b07fbeaf8338a3
eltonjtoledo <eltonjtoledo@gmail.com>
2020-09-02 21:48:51 -0300

    Create SelectionSort, InsertionSort, array with 100 random positions

--- sorting.js
+++ sorting.js
@@ -1,7 +1,7 @@
-const valuesa = [1, 5, 2, 8, 30, 57, 34, 09, 16, 83];
+const list = [1, 5, 2, 83, 8, 30, 57, 34, 09, 16];
 
 // Bubble sort
-function BubbleSorting(values){
+function BubbleSorting(values) {
 	let aux;
 	let cont = true;
 	for (let i = 0; i < values.length; i++) {
@@ -17,5 +17,60 @@ function BubbleSorting(values){
 	}
 	return values;
 }
-console.log(BubbleSorting(valuesa));
+//console.log(BubbleSorting(list));
+
+// Selection Sort
+function SelectionSort(values) {
+	for (let i = 0; i < values.length; i++) {
+		let lower = values[i];
+		for (let e = 0; e < values.length; e++) {
+			if (values[e] > lower) {
+				lower = values[e];
+				values[e] = values[i]
+				values[i] = lower;
+			}
+
+		}
+	}
+	return values;
+}
+// console.log(SelectionSort(list));
+
+// Insertion Sort
+function InsertionSort(values) {
+	let aux;
+	for (let i = 1; i < values.length; i++) {
+		let current = values[i];
+		let j = i - 1;
+
+		while (j >= 0 && values[j] > current) {
+			aux = values[j + 1];
+			values[j + 1] = values[j];
+			values[j] = aux;
+			j = j - 1;
+		}
+		values[j + 1] = current;
+	}
+	return values;
+}
+// console.log(InsertionSort(list));
+
+// To create a matrix with 100 random positions
+const generated = [];
+for (let i = 0; i < 100;) {
+	const value = Math.floor(Math.random() * 100 + 1);
+	if (!generated.includes(value)) {
+		generated.push(value);
+		i++;
+	}
+}
+
+console.log(generated ,InsertionSort(generated))
+
+
+
+
+
+
+
 
